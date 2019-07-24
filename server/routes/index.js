c4e18bfaecdf7b37f3d8494bc0ba1e07fdd389f0
Kafilat Abdulwahab <hadeneekeh01@gmail.com>
2019-07-24 17:35:57 +0100

    chore(api documentation): setup api documentation - api documentation using swagger UI [Delivers #167190429]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
 
 const router = express();
+
+const swaggerDocument = yaml.load('../docs/ah-commando-doc.yml');
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 router.get('/', (req, res) => res.status(200).json({
   status: res.statusCode,
