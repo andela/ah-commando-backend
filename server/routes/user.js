74c1825203d9e5777e8e6536ea85a251963da6a5
chuxmykel <ngwobiachukwudi@gmail.com>
2019-07-29 14:30:41 +0100

    feat(validations): implement input validations
    - install @hapi/joi library for input validation
    - validate sign in and sign up routes
    
    [Delivers #167190433]

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,15 +8,15 @@ const userRoute = express();
 const { validateToken, validateLogin, validateUser } = middlewares;
 
 
-const { signUp, login, logout, resetPassword, confirmEmail, sendResetLink } = UserController;
+const {
+  signUp, login, logout, resetPassword, confirmEmail, sendResetLink
+} = UserController;
 
-
-
+userRoute.post('/', validateUser, signUp);
+userRoute.post('/login', validateLogin, login);
 userRoute.get('/confirmEmail', confirmEmail);
 
 // logs out a user
-userRoute.post('/', signUp);
-userRoute.post('/login', login);
 userRoute.post('/logout', validateToken, logout);
 userRoute.post('/passwordReset/', sendResetLink);
 userRoute.put('/resetPassword/:id/:token', resetPassword);
