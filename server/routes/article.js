c6d641c65a8759003d8fd0d80036ce11ada22a37
Lundii <mondayemmanuel67@gmail.com>
2019-08-06 10:03:20 +0100

    ft(backend): adds search functionality
    users can search by keyword
    uset can search by keyword and/or categories,tags,authornames
    
    [Delivers #167190452]

--- server/routes/article.js
+++ server/routes/article.js
@@ -5,12 +5,16 @@ import middlewares from '../middlewares';
 const router = express.Router();
 
 const {
+<<<<<<< HEAD
   validateArticle,
   multerUploads,
   verifyToken,
   validateFilter,
   validateKeyword,
   validateHighlightData
+=======
+  validateArticle, multerUploads, verifyToken, validateFilter, validateKeyword
+>>>>>>> ft(backend): adds search functionality
 } = middlewares;
 
 const {
@@ -34,6 +38,7 @@ router.delete('/:slug', verifyToken, deleteArticle);
 router.post('/search/filter', validateFilter, getAllArticles);
 router.post('/:slug/highlight', verifyToken, validateHighlightData, highlightText);
 
+<<<<<<< HEAD
 // get and create a tag
 router.get('/tags/get', getAllTags);
 router.post('/tags/create', verifyToken, createTag);
@@ -41,4 +46,9 @@ router.post('/tags/create', verifyToken, createTag);
 // get and create a category
 router.get('/categories/get', getAllCategories);
 router.post('/categories/create', verifyToken, createCategory);
+=======
+// filters article search result based on selected filters
+router.post('/search/filter', validateFilter, getAllArticles);
+
+>>>>>>> ft(backend): adds search functionality
 export default router;
--- server/tests/comment.test.js
+++ server/tests/comment.test.js
@@ -277,6 +277,76 @@ describe('Handle Comment', () => {
             done();
           });
       });
+      before('add a post', (done) => {
+        chai
+          .request(app)
+          .post(`${baseUrl}/articles`)
+          .set('Authorization', `${userToken}`)
+          .send({
+            article: {
+              title: 'Hello',
+              description: 'i say hello',
+              articleBody: 'hhh',
+              tagList: 'jssd',
+              image: 'hs.jpg'
+            }
+          })
+          .end((err, res) => {
+            postId = res.body.articles.id;
+            done();
+          });
+      });
+      it('Should fail if user isn\'t logged in', (done) => {
+        chai
+          .request(app)
+          .post(`${baseUrl}/comment/${postId}`)
+          .send(commentData[0])
+          .end((err, res) => {
+            const { status, error } = res.body;
+            expect(status).to.equal(401);
+            expect(error).to.equal('Authorization error');
+            done();
+          });
+      });
+      it('Should fail if message is empty', (done) => {
+        chai
+          .request(app)
+          .post(`${baseUrl}/comment/${postId}`)
+          .set('Authorization', `${userToken}`)
+          .send(commentData[1])
+          .end((err, res) => {
+            const { status, error } = res.body;
+            expect(status).to.equal(400);
+            expect(error[0]).to.equal('comment is not allowed to be empty');
+            done();
+          });
+      });
+      it('Should fail if post is not found', (done) => {
+        chai
+          .request(app)
+          .post(`${baseUrl}/comment/${230}`)
+          .set('Authorization', `${userToken}`)
+          .send(commentData[0])
+          .end((err, res) => {
+            const { status, error } = res.body;
+            expect(status).to.equal(404);
+            expect(error).to.equal('Post not found');
+            done();
+          });
+      });
+      it('Should pass if user is a valid one and is logged in', (done) => {
+        chai
+          .request(app)
+          .post(`${baseUrl}/comment/${postId}`)
+          .set('Authorization', `${userToken}`)
+          .send(commentData[0])
+          .end((err, res) => {
+            const { status } = res.body;
+            expect(status).to.equal(201);
+            expect(res.body.comment).to.be.an('object');
+            done();
+          });
+      });
     });
   });
 });
